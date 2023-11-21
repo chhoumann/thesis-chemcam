43155288f38e8a73e361fbd38f3ea6805d250706
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-11-21 15:02:34 +0100

    add usage info

--- baseline/PLS_SM/utils.py
+++ baseline/PLS_SM/utils.py
@@ -5,6 +5,26 @@ import pandas as pd
 def custom_kfold_cross_validation(
     data, k: int, group_by: str, random_state=None
 ):
+    """
+    Perform custom k-fold cross-validation on the given data.
+
+    Parameters:
+    - data: The input data to be split into train and test sets.
+    - k: The number of folds for cross-validation.
+    - group_by: The column name to group the data by.
+    - random_state: The random seed for shuffling the data.
+
+    Returns:
+    - A generator that yields train and test sets for each fold.
+
+    Usage:
+        ```python
+        for train_data, test_data in custom_kfold_cross_validation(
+            final_df, k=5
+        ):
+            # Apply training and validation using train_data and test_data
+        ```
+    """
     kf = KFold(n_splits=k, shuffle=True, random_state=random_state)
 
     grouped = data.groupby(group_by)
@@ -23,6 +43,29 @@ def custom_kfold_cross_validation(
 def custom_train_test_split(
     data, group_by: str, test_size=0.2, random_state=None
 ):
+    """
+    Custom train-test split function that splits the data based on
+    a specified grouping variable.
+
+    Parameters:
+        data (pandas.DataFrame): The input data to be split.
+        group_by (str): The column name to group the data by.
+        test_size (float): The proportion of the data to be used for testing.
+        Default is 0.2.
+        random_state (int): The random seed for reproducibility.
+        Default is None.
+
+    Returns:
+        train_data (pandas.DataFrame): The training data.
+        test_data (pandas.DataFrame): The testing data.
+
+    Usage:
+        ```python
+        train_data, test_data = custom_train_test_split(
+            final_df, test_size=0.2
+        )
+        ```
+    """
     grouped = data.groupby(group_by)
     groups_keys = list(grouped.groups.keys())
 
