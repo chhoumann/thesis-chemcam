fe604d0f11ef14322111983ffc71b4299e0b82e7
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-12-07 09:01:46 +0100

    make plot fns

--- baseline/lib/plot.py
+++ baseline/lib/plot.py
@@ -4,13 +4,93 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import mlflow
 import pandas as pd
+import seaborn as sns
+from lib.data_handling import WavelengthMaskTransformer, get_preprocessed_sample_data
+from lib.reproduction import masks, spectrometer_wavelength_ranges
 
 
-def plot_spectra(df: pd.DataFrame):
-    pass
+def plot_spectra(
+    sample_name: str,
+    data_path: str,
+    average_shots=True,
+    max_gap_threshold=10,
+    fig_name="",
+):
+    sns.set_style("white")  # Set the Seaborn style
+    _data_path = Path(data_path)
+    assert _data_path.exists(), f"Data path {data_path} does not exist."
+
+    # Get the preprocessed sample data
+    sample_data = get_preprocessed_sample_data(
+        sample_name, _data_path, average_shots=average_shots
+    )
+    first_location = sample_data[0]
+
+    # Transform the data using WavelengthMaskTransformer
+    wmt = WavelengthMaskTransformer(masks)
+    transformed = wmt.fit_transform(first_location)
+
+    df = pd.DataFrame(
+        {"wave": transformed["wave"], "shot_avg": transformed["shot_avg"]}
+    )
+
+    # Debug: Print the DataFrame structure
+    print("DataFrame columns:", df.columns)
+
+    # Check if 'wave' and 'shot_avg' are in the DataFrame
+    if "wave" not in df.columns or "shot_avg" not in df.columns:
+        raise ValueError(
+            "Expected columns 'wave' and 'shot_avg' not found in the DataFrame."
+        )
+
+    # Extract wavelength and intensity data
+    wave, intensity = df["wave"], df["shot_avg"]
+
+    plt.figure(figsize=(10, 6))
+
+    # Plot segments with gaps smaller than max_gap_threshold
+    start_idx = 0
+    for i in range(1, len(wave)):
+        if wave.iloc[i] - wave.iloc[i - 1] > max_gap_threshold:
+            sns.lineplot(
+                x=wave.iloc[start_idx:i], y=intensity.iloc[start_idx:i], color="blue"
+            )
+            start_idx = i
+    sns.lineplot(
+        x=wave.iloc[start_idx:], y=intensity.iloc[start_idx:], color="blue"
+    )  # plot the last segment
+
+    # Colors for the spectral ranges
+    spectral_colors = {
+        "UV": "#BA55D3",  # Medium Orchid
+        "VIS": "#00FF00",  # Bright Green
+        "VNIR": "#8B0000",  # Deep Red
+    }
+
+    # Overlaying spectral ranges
+    for range_name, (start, end) in spectrometer_wavelength_ranges.items():
+        plt.axvspan(
+            start, end, alpha=0.3, color=spectral_colors[range_name], label=range_name
+        )
+
+    for i, (start, end) in enumerate(masks):
+        plt.axvspan(
+            start, end, alpha=0.1, color="black", label="Mask" if i == 0 else None
+        )
+
+    # Labeling and title
+    plt.title("Spectral Analysis of Sample: " + sample_name)
+    plt.xlabel("Wavelength (nm)")
+    plt.ylabel("Intensity")
+
+    plt.legend(title="Spectral Ranges")
+    plt.show()
+
+    if fig_name != "":
+        plt.savefig(fig_name, bbox_inches="tight")
 
 
-def plot_outliers_for_run(mlflow_run_id: str):
+def plot_outliers_for_run(mlflow_run_id: str, figure_name=""):
     mlflow.set_tracking_uri("http://localhost:5000")
     client = mlflow.MlflowClient()
 
@@ -48,12 +128,22 @@ def plot_outliers_for_run(mlflow_run_id: str):
     plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
 
     # Save the figure if needed
-    save_file_path = local_dir / "outlier_removal_over_time.png"
-    plt.savefig(save_file_path, bbox_inches="tight")
+    if figure_name != "":
+        save_file_path = local_dir / figure_name
+        plt.savefig(save_file_path, bbox_inches="tight")
 
     plt.show()
 
 
 if __name__ == "__main__":
-    # Example usage
-    plot_outliers_for_run("16b0b228ada94577b15b658890ea1dc4")
+    # Example
+    plot_outliers_for_run(
+        "16b0b228ada94577b15b658890ea1dc4", "outlier_removal_over_time"
+    )
+
+    # Example
+    plot_spectra(
+        "cadillac",
+        "data/data/calib/calib_2015/1600mm/pls",
+        fig_name="cadillac_spectra.png",
+    )
