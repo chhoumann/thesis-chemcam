3465017991bc2a1b1fde98551da29448c625a9b7
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-12-15 23:08:08 +0100

    assert

--- baseline/ica/data_processing.py
+++ baseline/ica/data_processing.py
@@ -68,6 +68,11 @@ class ICASampleProcessor:
     def postprocess(self, ica_estimated_sources: np.ndarray) -> None:
         columns = self.df.columns
 
+        assert len(columns) == len(ica_estimated_sources[0]) == 45, (
+            f"Expected 45 columns, got {len(columns)} in the data and "
+            f"{len(ica_estimated_sources[0])} in the estimated sources."
+        )
+
         corrcols = [f"IC{i+1}" for i in range(self.num_components)]
         df_ics = pd.DataFrame(
             ica_estimated_sources,
