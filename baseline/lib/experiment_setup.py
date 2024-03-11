e41bdc6b543463e7a96d65a0c03d41c8d2932475
Pattrigue <pfas19@student.aau.dk>
2024-03-11 13:49:12 +0100

    Update experiment_setup.py

--- baseline/lib/experiment_setup.py
+++ baseline/lib/experiment_setup.py
@@ -15,17 +15,14 @@ class Experiment:
             norm=norm
         )
 
-    def run(
+    def run_univariate(
         self,
         func: Callable[
             [pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, str, int], None
         ],
-        mlflow_set_experiment=True,
+        create_mlflow_experiment=True,
     ):
-        if mlflow_set_experiment:
-            mlflow.set_experiment(
-                f'{self.name}_Norm{self.norm}_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}'
-            )
+        self.mlflow_set_experiment(create_mlflow_experiment)
 
         for target in self.y_train.columns:
             with mlflow.start_run(run_name=f"{self.name}_{target}"):
@@ -40,6 +37,31 @@ class Experiment:
                     self.norm,
                 )
 
+    def run_multivariate(
+        self,
+        func: Callable[
+            [pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, int], None
+        ],
+        create_mlflow_experiment=True,
+    ):
+        self.mlflow_set_experiment(create_mlflow_experiment)
+
+        with mlflow.start_run(run_name=f"{self.name}"):
+            mlflow.log_param("norm", self.norm)
+            func(
+                self.X_train,
+                self.X_test,
+                self.y_train,
+                self.y_test,
+                self.norm,
+            )
+
     def mlflow_sklearn_log_model(self, model: BaseEstimator, *args: str):
         name = f"{self.name}_{self.norm}{'_' + '_'.join(args) if args else ''}"
         mlflow.sklearn.log_model(model, name)
+
+    def mlflow_set_experiment(self, create_mlflow_experiment: bool):
+        if create_mlflow_experiment:
+            mlflow.set_experiment(
+                f'{self.name}_Norm{self.norm}_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}'
+            )
