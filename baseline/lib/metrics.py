7653226e978cdd2c7f6c89dd05ec6257b1dddd80
Christian Bager Bach Houmann <christian@bagerbach.com>
2024-06-19 07:52:07 +0200

    visualize data partitioning

--- baseline/lib/metrics.py
+++ baseline/lib/metrics.py
@@ -8,3 +8,11 @@ def rmse_metric(y_true, y_pred):
 
 def std_dev_metric(y_true, y_pred):
     return float(np.std(y_true - y_pred, ddof=1))
+
+def error_consistency(y_true, y_pred):
+    abs_errors = np.abs(y_true - y_pred)
+    mean_abs_error = np.mean(abs_errors)
+    squared_diffs = (abs_errors - mean_abs_error) ** 2
+    error_consistency = np.sqrt(np.sum(squared_diffs) / (len(y_true) - 1))
+    
+    return error_consistency
